f65e45a0f12f753aec00086ce35a9d77bfec1982
Samyrov <hackercoock@gmail.com>
2023-05-18 00:32:05 +0300

    practic 4

---
+++ lesson4/practic 4.py
@@ -0,0 +1,22 @@
+sum=int(input("Яку суму бажаєте зняти "))
+notes = [500, 200, 100, 50, 20, 10] 
+counts = [0] * len(notes) 
+remainder = sum 
+
+for i in range(len(notes)):
+    if remainder == 0:
+        break
+    elif remainder < notes[i]:
+        continue
+    else:
+        count = min(remainder // notes[i], 10) 
+        counts[i] = count
+        remainder -= count * notes[i]
+
+if remainder < 0:
+    print("Неможливо видати суму максимальними банкнотами.")
+else:
+    print("Максимальна кількість банкнот:")
+    for i in range(len(notes)):
+        if counts[i] > 0:
+            print(str(notes[i]) + " x " + str(counts[i]))
